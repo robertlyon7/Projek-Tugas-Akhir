742cb72c0f51ad7774c0571dad1a94794ebdfd0f
tiarrasyid <tiarrasyid2298@gmail.com>
2023-03-03 21:49:11 +0700

    [TR] Proses CRUD

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,8 +3,7 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://127.0.0.1:8000" });
-
+const api = axios.create({ baseURL: "http://api.seele.my.id/" });
 export default boot(({ app, store }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
